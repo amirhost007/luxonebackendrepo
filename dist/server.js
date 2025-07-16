a7985844e9a64e98bf134e111ef016c1fbf365f7
Future Tech Admin <amirhost007@gmail.com>
2025-07-17 00:07:58 +0530

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -8,11 +8,15 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: ['http://localhost:3000'], // Add production domain if needed
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+    origin: [
+      'http://localhost:3000',
+      'https://luxoneonlinequotation.vercel.app'
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }));
+  
 
 app.use(express.json());
 
