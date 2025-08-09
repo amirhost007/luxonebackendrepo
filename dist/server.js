8417c2501c7ca1383efee296ba19b9a9c3089078
Future Tech Admin <amirhost007@gmail.com>
2025-08-09 15:39:30 +0530

    Update server.js

--- dist/server.js
+++ dist/server.js
@@ -20,5 +20,5 @@ app.get('/health', (req, res) => {
 app.use('/api', routes_1.default);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server running on https://luxonebackendrepo.onrender.com:${PORT}`);
 });
