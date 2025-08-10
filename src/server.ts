616a5b5badf9ac0de5a7f7fb378e0a4d95f02881
Future Tech Admin <amirhost007@gmail.com>
2025-08-11 01:07:55 +0530

    Fix CORS configuration to allow quotation.theluxone.com domain

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,15 @@ dotenv.config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+  origin: [
+    'http://localhost:3000', 
+    'http://localhost:5173', 
+    'http://localhost:5174', 
+    'http://127.0.0.1:5173', 
+    'http://127.0.0.1:5174',
+    'https://quotation.theluxone.com',
+    process.env.CORS_ORIGIN
+  ].filter(Boolean) as string[],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
