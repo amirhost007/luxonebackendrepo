e2545f382687441859032d19e540d980226f18f6
Amir Host Web <83163078+amirhost007@users.noreply.github.com>
2025-08-11 01:09:40 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,7 @@ app.use(cors({
     'http://localhost:5174', 
     'http://127.0.0.1:5173', 
     'http://127.0.0.1:5174',
-    'https://quotation.theluxone.com',
+    'https://quotation.theluxone.com/login',
     process.env.CORS_ORIGIN
   ].filter(Boolean) as string[],
   credentials: true,
